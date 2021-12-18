b3bfff9124ea9a4055cb5ee91ee5ee863c628ad5
DNX2461 <david.quoccuong2001@gmail.com>
2021-12-18 10:55:31 +0700

    fix xong 5 tới 8

---
+++ fix/3_T2_LostUpdate_error.sql
@@ -0,0 +1,2 @@
+﻿EXEC dbo.UPDATE_DONHANG @MADH = 'HD0000000001', 
+	@QTVC = N'Đang vận chuyển'
---
+++ fix/5_T2_UpdateLost_fix.sql
@@ -0,0 +1,5 @@
+﻿SELECT * FROM dbo.QUANLYKHO
+EXEC dbo.INSERT_GHINHAN @MADH = 'HD0000000003',  -- char(12)
+                        @MASP = 'SP0000000001',  -- char(12)
+                        @SOLUONG = 300 -- int
+GO
